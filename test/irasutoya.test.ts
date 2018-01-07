62dbc41411ac33655da84ee44618dc8f5f08b718
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2018-01-08 02:37:57 +0900

    Replace mocha + powerassert with jest

---
+++ test/irasutoya.test.ts
@@ -0,0 +1,10 @@
+import "jest";
+import * as irasutoya from "../src/irasutoya";
+
+test("randomImage", async () => {
+  const detailImage = await irasutoya.randomImage();
+  expect(detailImage.title).not.toBe("");
+  expect(detailImage.imageUrl).not.toBe("");
+  expect(detailImage.description).not.toBe("");
+  expect(detailImage.categories).not.toBe("");
+});
